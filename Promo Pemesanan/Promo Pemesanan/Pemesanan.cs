634f8864dc289ce243cfba3daf2811258b276963
anggunnn <118151036+autumx@users.noreply.github.com>
2023-06-23 21:13:30 +0700

    Add files via upload

---
+++ Promo Pemesanan/Promo Pemesanan/Pemesanan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Promo_Pemesanan
+{
+    public partial class Pemesanan : Form
+    {
+        public Pemesanan()
+        {
+            InitializeComponent();
+        }
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Pemesanan_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
